94d88b15b828711a6ecf8222494c2ac63d417a72
Sreedhar Nambi <94751973+SreedharNambi@users.noreply.github.com>
2022-02-01 18:12:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="main-container">
+    <h1 className="header">Super Over League</h1>
+    <div className="img-container">
+      <img
+        className="imageStyle"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="imageStyle"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
